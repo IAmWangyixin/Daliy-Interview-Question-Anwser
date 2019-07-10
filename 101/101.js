308b20e45e8765607cd5da87f647e1fe21d549ac
wangyimei <nina.wangyimei@gmail.com>
2019-07-10 10:51:34 +0800

    add analysis and complete demo

--- 101/101.js
+++ 101/101.js
@@ -1,13 +1,36 @@
-function print(n) {
-    setTimeout(() => {
-        console.log(n);
-    }, Math.floor(Math.random() * 1000));
-}
+/**
+ * 4-8 与 9-15 输出一致。
+ * setTimeout调用中，变量沿着定义作用域向上查找。
+ * setTimeout回调函数被按照delay参数的大小，依次插入事件队列。
+ */
+// function print(n){
+//   setTimeout(() => {
+//     console.log(n);
+//   }, Math.floor(Math.random() * 1000));
+// }
+// function print(n) {
+//     console.log('fun:', n)
+//     setTimeout(function(){
+//         console.log(n);
+//         // return () => {};
+//     }, Math.floor(Math.random() * 1000));
+// }
+// function print(n){
+//   setTimeout(() => {
+//     console.log(n);
+//   }, 1, Math.floor(Math.random() * 1000));
+// }
 
 function print(n) {
-    setTimeout(() => {
+    console.log('fun:', n)
+    /* setTimeout((() => {
         console.log(n);
-    }, 1, Math.floor(Math.random() * 1000));
+        return () => {};
+    }).apply(n), Math.floor(Math.random() * 1000)); */
+    setTimeout((() => {
+        console.log(n);
+        return () => {};
+    })(), Math.floor(Math.random() * 1000));
 }
 for (var i = 0; i < 10; i++) {
     print(i);
